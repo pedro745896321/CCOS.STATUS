9be3717273f47db8bd33a22d74e717736984411f
pedro745896321 <pedroassfernandes.25@gmail.com>
2025-12-30 02:47:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,26 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
+  // Carrega variáveis de ambiente
+  loadEnv(mode, process.cwd(), '')
+
+  return {
+    // IMPORTANTE para Vercel / GitHub
     base: './',
-        
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+
+    plugins: [react()],
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+    },
+  }
+})
